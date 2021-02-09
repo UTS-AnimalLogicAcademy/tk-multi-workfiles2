04a02de7c0a548d53fe6c5a2134563cde6b5409b
Naomi Que <149199@ocelot.ala.int.uts.edu.au>
2021-02-09 13:35:46 +1100

    Added package.py and CMakeLists.txt to rez-build

---
+++ package.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+name = 'tk_multi_workfiles2'
+
+version = '0.8.00.00'
+
+description = \
+    """
+    utsala fork of tk-multi-workfiles2 app_store bundle
+    """
+
+
+def commands():
+    env.TK_MULTI_WORKFILES2_BUNDLE = '{this.root}'
+
+
+uuid = 'f0d31115741449fda8f68960d86721db'
+
+timestamp = 1490324515
